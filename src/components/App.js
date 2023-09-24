b7ab56d589b0e2116da01510dc8d6947398fe6a0
r3workz <122603849+r3workz@users.noreply.github.com>
2023-09-24 16:41:43 +0000

    abc

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+const list = ["relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative","relative"]
 const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key="relativeList">
+                {
+                  list.map((rel, index)=>{
+                    return <li key={`relativeListItem${index + 1}`}>{rel}</li>
+                  })
+                }
+               </ol>
     </div>
   )
 }
